f6edc316d120d8154fdb6018cab41ce99b3a2e12
unknown <joseph.martino.iufs@gmail.com>
2023-03-16 17:20:15 +0100

    liquibase changelog configuration uploaded
    csv parsing updated
    
    author repository update
    
    repositories added
    
    csv parser updated
    
    repositories class updated to interface using crudRepository, csv parser add and get data from database
    
    restController created, create, getById, getAllBooks methods added
    
    dto, restcontroller restored

---
+++ src/main/java/com/mylib/core/dto/AuthorDto.java
@@ -0,0 +1,31 @@
+package com.mylib.core.dto;
+
+import java.util.List;
+
+
+public class AuthorDto {
+
+	private long id;
+	private String fullName;
+	private List<BookDto> books; 
+	
+
+	public List<BookDto> getBooks() {
+		return books;
+	}
+	public void setBooks(List<BookDto> books) {
+		this.books = books;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+}
